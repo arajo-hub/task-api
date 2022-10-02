5429fa177a4dbcbd2809d40924a38b91229f3ce6
Ara Jo <joara9566@naver.com>
2022-10-02 23:22:29 +0900

    프로젝트 생성

---
+++ src/main/java/com/kmong/api/KmongapiApplication.java
@@ -0,0 +1,13 @@
+package com.kmong.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmongapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmongapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmong/api/KmongapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmong.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmongapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
