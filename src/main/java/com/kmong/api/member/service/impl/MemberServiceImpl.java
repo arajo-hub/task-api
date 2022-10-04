448db117120aa7024db99211a9067769bfc50fed
Ara Jo <joara9566@naver.com>
2022-10-04 20:01:26 +0900

    회원 응답 변경

--- src/main/java/com/kmong/api/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/kmong/api/member/service/impl/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package com.kmong.api.member.service.impl;
 import com.kmong.api.member.domain.Member;
 import com.kmong.api.member.repository.MemberRepository;
 import com.kmong.api.member.request.MemberCreate;
+import com.kmong.api.member.response.MemberView;
 import com.kmong.api.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,8 +24,17 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
-    public ResponseEntity join(MemberCreate memberCreate) {
+    public ResponseEntity<MemberView> join(MemberCreate memberCreate) {
+        ResponseEntity response = new ResponseEntity(HttpStatus.OK);
         Member member = memberCreate.toMember();
+        Optional<Member> savedResult = memberRepository.save(member);
+        if (savedResult.isPresent()) {
+            MemberView memberView = savedResult.get().toMemberView();
+            response = new ResponseEntity(memberView, HttpStatus.OK);
+        } else {
+            response = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
         return new ResponseEntity(memberRepository.save(member), HttpStatus.OK);
     }
 }
