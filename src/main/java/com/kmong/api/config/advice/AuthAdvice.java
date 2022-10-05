62eb295c08f0ac4efc5a4f15cf57ef11bd60498e
Ara Jo <joara9566@naver.com>
2022-10-05 20:20:04 +0900

    권한 체크 후 보내는 응답 변경

--- src/main/java/com/kmong/api/config/advice/AuthAdvice.java
+++ src/main/java/com/kmong/api/config/advice/AuthAdvice.java
@@ -1,5 +1,6 @@
 package com.kmong.api.config.advice;
 
+import com.kmong.api.common.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,8 @@ public class AuthAdvice {
 
     @ExceptionHandler(IllegalAccessException.class)
     public ResponseEntity deniedAccess() {
-        return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        Response response = new Response("401", "로그인 후 이용 가능합니다.");
+        return new ResponseEntity(response, HttpStatus.UNAUTHORIZED);
     }
 
 }
