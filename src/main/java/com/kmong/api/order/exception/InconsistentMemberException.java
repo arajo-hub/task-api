3a4314ffb592df4217c9d5e0d76f668f295e2015
Ara Jo <joara9566@naver.com>
2022-10-07 21:15:16 +0900

    본인이 아닌 멤버아이디로 주문시 예외 처리

--- src/main/java/com/kmong/api/order/exception/InconsistentMemberException.java
+++ src/main/java/com/kmong/api/order/exception/InconsistentMemberException.java
@@ -4,7 +4,7 @@ import com.kmong.api.common.exception.KmongApiException;
 
 public class InconsistentMemberException extends KmongApiException {
 
-    private static final String MESSAGE = "조회권한이 없습니다. 본인의 주문내역만 조회 가능합니다.";
+    private static final String MESSAGE = "조회권한이 없습니다.";
 
     public InconsistentMemberException() {
         super(MESSAGE);
--- src/main/java/com/kmong/api/order/repository/OrderRepository.java
+++ src/main/java/com/kmong/api/order/repository/OrderRepository.java
@@ -45,7 +45,7 @@ public class OrderRepository {
 
     /**
      * 주문내역 전체 조회
-     * @param id 주문내역을 조회할 회원 아이디
+     * @param orderSearch 주문내역을 조회할 정보
      * @return 주문내역 리스트
      */
     public List<Order> findAllOrder(OrderSearch orderSearch) {
